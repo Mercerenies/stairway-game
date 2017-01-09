1d2561580b136b437af3e15b424488f49775783d
Mercerenies <sdmayolo42@students.tntech.edu>
2017-01-08 22:17:21 -0600

    A few small modifications

--- stairway/product/FruitProduct.scala
+++ stairway/product/FruitProduct.scala
@@ -1,6 +1,8 @@
 
 package com.mercerenies.stairway.product
 
+import com.mercerenies.stairway.image.FruitsImage
+
 abstract class FruitProduct extends Purchasable with Usable with Captioned {
 
   override def dims: (Double, Double) = (FruitsImage.Width, FruitsImage.Height)
--- stairway/space/MysterySpace.scala
+++ stairway/space/MysterySpace.scala
@@ -1,12 +1,14 @@
 
 package com.mercerenies.stairway.space
 
+import com.mercerenies.stairway.util
 import com.mercerenies.stairway.game.StandardGame
 import com.mercerenies.stairway.game.content.MysteryContent
 import com.mercerenies.stairway.luck.DiceValue
 import com.mercerenies.stairway.product.Fruits
 
 case class MysterySpace(val count: Int) extends ImageSpace {
+  import util.RandomImplicits._
 
   override def imageIndex: Int = 13
 
@@ -22,9 +24,8 @@ case class MysterySpace(val count: Int) extends ImageSpace {
     // These spaces get rarer later on, so it's fairly safe to emulate
     // them by assuming the player will get something (it will balance
     // out with the fruit spaces, whose emulation is ignored)
-    Fruits.apple.giveTo(master.player)
-    Fruits.orange.giveTo(master.player)
-    Fruits.melon.giveTo(master.player)
+    val chosen = util.rand.nextOf(Fruits.apple, Fruits.orange, Fruits.melon)
+    chosen.giveTo(master.player)
   }
 
 }

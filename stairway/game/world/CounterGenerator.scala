ce4f442119fee40fddfcdaee06e5986e231be124
Mercerenies <sdmayolo42@students.tntech.edu>
2017-01-08 22:16:43 -0600

    New convenience generator

---
+++ stairway/game/world/CounterGenerator.scala
@@ -0,0 +1,19 @@
+
+package com.mercerenies.stairway.game.world
+
+import com.mercerenies.stairway.game.belt.ConveyerFeed
+import com.mercerenies.stairway.util.{Index, RandomImplicits}
+
+trait CounterGenerator[+T <: ConveyerFeed] extends Generator[T] {
+  import RandomImplicits._
+
+  private var _counter = 0
+
+  def counter = _counter
+
+  abstract override def trigger(next: Index): Unit = {
+    super.trigger(next)
+    _counter += 1
+  }
+
+}

49607bd7a50d24dd3a0dcd7e28f32d37d90d58e7
Mercerenies <sdmayolo42@students.tntech.edu>
2017-01-09 19:58:35 -0600

    Luck events which are 0% or 100% do not change karma now

--- stairway/luck/PlayerLuck.scala
+++ stairway/luck/PlayerLuck.scala
@@ -22,14 +22,22 @@ class PlayerLuck(val master: StandardGame.Master) {
   def luck: Double = math.max(baseLuck + karma * multiplier, 0.0)
 
   def evaluateLuck(weight: (Double, Double), odds: Double): Boolean = {
-    // This also adjusts the karma from the event
-    // Both weight arguments should be nonnegative
-    val winner = (rand.nextDouble() <= odds * luck)
-    if (winner)
-      karma -= weight._1
-    else
-      karma += weight._2
-    winner
+    // If an event was already determined conclusively without random chance,
+    // go with that result without factoring in luck or karma.
+    if (odds >= 1.00) {
+      true
+    } else if (odds <= 0.00) {
+      false
+    } else {
+      // This also adjusts the karma from the event
+      // Both weight arguments should be nonnegative
+      val winner = (rand.nextDouble() <= odds * luck)
+      if (winner)
+        karma -= weight._1
+      else
+        karma += weight._2
+      winner
+    }
   }
 
   def evaluateLuck(weight: Double, odds: Double): Boolean = {

37869747571b8f352c1ac15837b3bab32f19954f
Mercerenies <sdmayolo42@students.tntech.edu>
2017-01-10 21:50:28 -0600

    Enemies give more money now

--- stairway/enemy/Rat.scala
+++ stairway/enemy/Rat.scala
@@ -6,7 +6,7 @@ import com.mercerenies.stairway.game.{Player, StandardGame}
 class Rat(master: StandardGame.Master, entropy: Enemy.Entropy)
     extends SingleEnemy(master) {
 
-  override def spoils: Spoils = Spoils.Money((13 + 2 * entropy.reward).toInt)
+  override def spoils: Spoils = Spoils.Money((16 + 2 * entropy.reward).toInt)
 
   override def startingHealth: Double = 5.0 + 1.0 * entropy.risk
 
